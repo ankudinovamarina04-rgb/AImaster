04a280a51bd83aca7de175324cc80cfb8aa9a2d3
ankudinovamarina04-rgb <ankudinovamarina04@gmail.com>
2025-10-30 04:39:17 +0700

    Initialize config.py with TOKEN and WEBHOOK_URL
    Added configuration for TOKEN and WEBHOOK_URL.

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# Укажи свои данные:
+TOKEN = os.getenv(8041869763:AAHp2urfzpk-NrpXc6HeW6CU9E_L6GSVnCk)
+WEBHOOK_URL = os.getenv(https://script.google.com/macros/s/AKfycby50wbLo_Tl6C0v-1mKYtBjtawY07sVD6TjHtDWmk6eMclGg-dgijTBJ38hfuaPEmrK/exec)
